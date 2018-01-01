2e143324d8beaefea22f53e49307c496a32a9445
Jay Wengrow <jaywngrw@gmail.com>
2018-01-01 15:09:27 -0600

    more refactoring

--- app/controllers/api/v1/codes_controller.rb
+++ app/controllers/api/v1/codes_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::CodesController < ApplicationController
   def analyze
     code_analyzer = CodeAnalyzer.new(params[:code])
     
-    render json: {count: code_analyzer.number_of_steps}
+    render json: {count: code_analyzer.results}
   end
 end
--- app/models/code_analyzer.rb
+++ app/models/code_analyzer.rb
@@ -2,20 +2,32 @@ class CodeAnalyzer
   
   def initialize(code='')
     @code = code
+    add_counters_to_code!
+    @results = []
   end
 
+  def results
+    return number_of_steps
+  end
+
+  private
+
   def number_of_steps
+    begin
+      return eval(@code)
+    rescue
+      return "Error: Code doesn't run!"
+    end
+  end
+
+  def add_counters_to_code!
     new_code = "count = 0\n"
     @code.each_line do |line|
       new_code += "#{line}\n"
       new_code += "count += 1\n"
     end
     new_code += "count"
-    begin
-      return eval(new_code)
-    rescue
-      return "Error: Code doesn't run!"
-    end
+    @code = new_code
   end
 
 end
