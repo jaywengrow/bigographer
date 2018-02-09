57138680fa7bfee84e18e98c8300dbeddef622c3
ABS19stein <abs19stein@gmail.com>
2018-02-08 18:55:50 -0600

    added test that tests multiple elements in a an array

--- test/models/code_analyzer_test.rb
+++ test/models/code_analyzer_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
- 
+
 class CodeAnalyzerTest < ActiveSupport::TestCase
-  
+
   test 'initializing CodeAnalyzer adds counting code to submitted code' do
     code_analyzer = CodeAnalyzer.new('x = 1')
     assert_equal "count = 0\nx = 1\ncount += 1\ncount", code_analyzer.code
@@ -25,5 +25,10 @@ class CodeAnalyzerTest < ActiveSupport::TestCase
   test '#results - returns complete graph data' do
     code_analyzer = CodeAnalyzer.new("[*].each do |number|\nnumber\nend")
     assert_equal [{x: 100, y: 201}, {x: 500, y: 1001}, {x: 1000, y: 2001}, {x: 1500, y: 3001}, {x: 2000, y: 4001}, {x: 2500, y: 5001}, {x: 3000, y: 6001}], code_analyzer.results
+  end
+
+  test '#run_code - that processes multiple elements in an array' do
+    analyzer = CodeAnalyzer.new("[[9].each do |number|\nsum += number #ARRRRRRRRRRRR\nend, var = 'Im a pirate!'\n#testing]")
+    assert_equal analyzer.codes,["count = 0\n[[9].each do |number|\n\ncount += 1\nsum += number #ARRRRRRRRRRRR\n\ncount += 1\nend\ncount += 1\ncount", "count = 0\n var = 'Im a pirate!'\n\ncount += 1\ncount"]
   end
 end
